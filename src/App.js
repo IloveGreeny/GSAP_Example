c143b8064844b56957e3be30e9e94f01a6d56deb
IloveGreeny <greenxd23@gmail.com>
2022-07-04 19:15:01 +0400

    footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Heading from "./Components/Heading";
+import Footer from "./Components/Footer";
 
 export default function App() {
   return(
       <div>
           <Heading/>
+          <h1>free</h1>
+          <Footer/>
       </div>
   )
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,49 @@
+import * as gsap from "gsap";
+import "./Heading.css";
+import logo from "../Logos/logo.svg"
+import { useRef, useEffect } from "react";
+
+export default function Footer() {
+    let logoItem = useRef(null);
+    let textItem = useRef(null);
+    useEffect(() => {
+        return () => {
+            logoItem.style.backgroundColor = "black"
+            gsap.TweenMax.to(
+                logoItem,
+                .8,
+                {
+                    opacity: 1,
+                    x: 840,
+                    ease: gsap.Power3.easeInOut,
+                }
+            )
+            gsap.TweenMax.to(
+                textItem,
+                .8,
+                {
+                    opacity: 1,
+                    y: -100,
+                    ease: gsap.Power3.easeInOut,
+                    marginRight: 20,
+                }
+            )
+        };
+    }, []);
+
+
+    return (
+        <div>
+            <header className="div">
+                <a href="#"><img
+                    width="200px"
+                    className="image"
+                    ref={el => {logoItem = el}}
+                    alt="logo"
+                    src={logo}/></a>
+                <p ref={el => {textItem = el}}
+                   className="paragraph"> About  Contact  Heading </p>
+            </header>
+        </div>
+    )
+}
